823c942dc0ef8a844a94303e662405de00d5936b
Liam Tinney <ltinney@powerhrg.com>
2020-06-12 07:40:47 -0400

    Done.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -23,14 +23,14 @@ class VendingMachine
     end
   end
 
-  def seats_available?(origin_index, destination_index, qty_of_tickets)
+  def seats_available?(origin_index, destination_index, qty_of_tix)
     route[origin_index...destination_index].each do |station|
-      return false if station["remaining seats"] < qty_of_tickets
+      return false if station["remaining seats"] < qty_of_tix
     end
     true
   end
 
-  def purchase_tickets(this_destination, qty_of_tickets, name)
+  def purchase_tickets(this_destination, qty_of_tix, name)
     this_origin = self.stationed_at
     origin_index = find_index(this_origin)
     destination_index = find_index(this_destination)
@@ -41,28 +41,28 @@ class VendingMachine
        flipped = true
     end
 
-    if seats_available?(origin_index, destination_index, qty_of_tickets)
-      adjust_num_of_remaining_seats(origin_index, destination_index, qty_of_tickets)
+    if seats_available?(origin_index, destination_index, qty_of_tix)
+      adjust_num_of_remaining_seats(origin_index, destination_index, qty_of_tix)
       if flipped
         origin_index, destination_index = destination_index, origin_index
       end
-      get_tickets(this_origin, this_destination, name, qty_of_tickets)
+      get_tix(this_origin, this_destination, name, qty_of_tix)
       "Transaction completed, thank you for choosing Amtrak."
     else
       "Tickets can't be purchased because there are not enough seats. We aplogize for the inconvenience."
     end
   end
 
-  def adjust_num_of_remaining_seats(origin_index, destination_index, qty_of_tickets)
+  def adjust_num_of_remaining_seats(origin_index, destination_index, qty_of_tix)
     self.route.each_with_index do |station, i|
       if origin_index <= i && i <= destination_index
-        station["remaining seats"] -= qty_of_tickets
+        station["remaining seats"] -= qty_of_tix
       end
     end
   end
 
-  def get_tickets(origin, destination, name, qty_of_tickets)
-    qty_of_tickets.times do
+  def get_tix(origin, destination, name, qty_of_tix)
+    qty_of_tix.times do
       self.tickets << Ticket.new(origin, destination, name)
     end
   end
